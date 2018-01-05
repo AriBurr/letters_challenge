c6951a9706792979a1a3595433f9617e5f834b51
Ariel Burriss <ariburr@gmail.com>
2018-01-05 10:21:08 -0700

    clean up

--- letters.rb
+++ letters.rb
@@ -1,21 +1,22 @@
 def letters_challenge
   puts "Please input a list of letters (ex. A, b, C, d)"
-  string = gets.strip
-  convert_to_ascii(string)
+  convert_to_ascii(gets.strip)
 end
 
 def convert_to_ascii(string)
-  strArr = string.gsub(" ", "").split(",")
-  asciiArr = []
+  arr = string.gsub(" ", "").split(",")
+  puts arr.map { |letter| { "#{letter}" => letter.ord } }
+  try_again
+end
 
-  strArr.map do |letter|
-    ascii = Hash.new
-    ascii["#{letter}"] = letter.ord
-    asciiArr.push(ascii)
+def try_again
+  puts "Would you like to try again:\n[1] Yes\n[2] No"
+  case gets.strip.to_i
+    when 1
+      letters_challenge
+    when 2
+      exit
   end
-
-  puts asciiArr
-
 end
 
 letters_challenge

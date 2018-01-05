0ebb34051e41707d023031b25f4b018a1411d785
Ariel Burriss <ariburr@gmail.com>
2018-01-05 10:38:16 -0700

    add asc and desc sorting methods

--- letters.rb
+++ letters.rb
@@ -5,18 +5,32 @@ end
 
 def convert_to_ascii(string)
   arr = string.gsub(" ", "").split(",")
-  puts arr.map { |letter| { "#{letter}" => letter.ord } }
-  try_again
+  ascii_arr = arr.map { |letter| { "#{letter}" => letter.ord } }
+  puts ascii_arr
+  try_again(ascii_arr)
 end
 
-def try_again
-  puts "Would you like to try again:\n[1] Yes\n[2] No"
+def sort_asc(arr)
+  puts arr.sort_by(&:to_a)
+end
+
+def sort_desc(arr)
+  puts arr.sort_by(&:to_a).reverse!
+end
+
+def try_again(arr)
+  puts "Would you like to try new numbers:\n[1] Yes\n[2] Sort ASC\n[3] Sort DESC\n[4] Quit"
   case gets.strip.to_i
     when 1
       letters_challenge
     when 2
+      sort_asc(arr)
+    when 3
+      sort_desc(arr)
+    when 4
       exit
   end
+  try_again(arr)
 end
 
 letters_challenge
